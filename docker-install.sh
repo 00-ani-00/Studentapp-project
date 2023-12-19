fc23923c308cbc23cb5a04b01ddb2ea51fc5a22b
00-ani-00 <aniketlagad6@gmail.com>
2023-12-19 12:03:36 +0530

    jenkins port forward command add

--- docker-install.sh
+++ docker-install.sh
@@ -5,3 +5,14 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu   #my case is ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+
+##for forward for becaus jenkins run on 8080 and tomcat also 8080 so jenkin forward to 8081
+sudo systemctl stop jenkins
+sudo systemctl status jenkins
+cd /etc/default
+sudo vi jenkins   #chnage port HTTP_PORT=8090 and save and exit
+cd /lib/systemd/system
+sudo vi jenkins.service  #change Environments="Jenkins_port=8090" save and exit
+sudo systemctl daemon-reload
+sudo systemctl restart jenkins
+sudo systemctl status jenkins
